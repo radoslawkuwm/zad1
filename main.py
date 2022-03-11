7541cca6ecf998b4c4662716130cdd8a6ca2449b
rados <radoslawkazimierczuk@gmail.com>
2022-03-11 19:22:16 +0100

    Initial commit

---
+++ main.py
@@ -0,0 +1,58 @@
+import math
+# def zad1():
+#     a=1
+#     b=2
+#     c=2.5
+#     d=3.5
+#     e='jeden'
+#     f='dwa'
+#     g=2+4j
+#     h=3+2j
+#     print(a,b,c,d,e,f,g,h)
+
+# def zad2(a,b):
+#     print(a+b)
+#     print(a - b)
+#     print(a * b)
+#     print(a / b)
+
+# def zad3(a):
+#     a+=1
+#     print(a)
+#     a -= 2
+#     print(a)
+#     a/=3
+#     print(a)
+#     a *= 4
+#     print(a)
+#     a **= 5
+#     print(a)
+#     a %= 2
+#     print(a)
+
+# def zad4():
+#     print(math.exp(10))
+#     print(math.pow(math.log( (math.sin(8)**2)+5,math.e),1/6))
+
+# def zad5():
+#     imie='RADOSLAW'
+#     nazwisko='KAZIMIERCZUK'
+#     print(imie.capitalize(),nazwisko.capitalize())
+
+# def zad6():
+#     coco = "ya ya ya coco jamboo, ya ya yeah ya ya ya coco jamboo, ya ya yeah"
+#     print(coco.count('ya'))
+
+# def zad7():
+#     napis='Wizualizacja'
+#     print(napis[1],napis[-1])
+
+# def zad8():
+#     coco = "ya ya ya coco jamboo, ya ya yeah Ya ya ya coco jamboo, ya ya yeah"
+#     print(coco.split(','))
+
+# def zad9():
+#     a = 'string'
+#     b = 2.5
+#     c = 0XFF
+#     print(a,b,c)
